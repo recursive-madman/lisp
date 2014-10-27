17063a0be8b0576cfc7e7144e2d2d4b989604050
Recursive Madman <recursive.madman@gmx.de>
2014-10-27 20:43:50 +0100

    lisp/alist: no custom assertions.

--- alist.c
+++ alist.c
@@ -1,18 +1,11 @@
 
 #include "lisp.h"
 
-#define ASSERT_CONS(exp) {                      \
-    if(exp->type != LISP_CONS) {                \
-      fprintf(stderr, "No cons!\n");            \
-      abort();                                  \
-    }                                           \
-  }
-
 LispExpression *lisp_alist_find(LispExpression *alist, char *symbol) {
   if(alist->type == LISP_NIL) {
     return NULL;
   } else {
-    ASSERT_CONS(alist);
+    LISP_ASSERT_TYPE(alist, LISP_CONS);
     LispCons cons = alist->value.cons;
     LispCons left = cons.left->value.cons;
     if(strcmp(symbol, left.left->value.symbol) == 0) {
