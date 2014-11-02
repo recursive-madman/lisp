0a64373d120a70baed8e9684399a0ccabb30b376
Recursive Madman <recursive.madman@gmx.de>
2014-11-02 15:24:06 +0100

    read & write history from ~/.lisp_history

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/history.h>
 
 #include "lisp.h"
+
+char history_path[256];
+
+void save_history() {
+  write_history(history_path);
+}
 
 int main(int argc, char **argv) {
 # ifdef LISP_DEBUG_MEMORY
   mdbg_init(0);
 # endif
+  using_history();
+  snprintf(history_path, 256, "%s/.lisp_history", getenv("HOME"));
+  read_history(history_path);
+  atexit(save_history);
   LispContext *ctx = lisp_context_create(NULL);
   lisp_install_functions(ctx);
   lisp_repl(ctx, stdin, stdout);
